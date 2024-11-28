30d06aa315e3fed65d891aa8da404e2d07a9d3e7
Yerfdog1990 <gouma308@gmail.com>
2024-11-29 04:43:37 +0700

    forEach terminal operation.

---
+++ StreamAPI/src/TerminalOperations/forEach.java
@@ -0,0 +1,27 @@
+package TerminalOperations;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class forEach {
+    public static void main(String[] args) {
+        List<String> list = List.of("a", "b", "c");
+        System.out.printf("Original list: %s%n", list);
+
+        // forEach operation
+        System.out.println("forEach operation:");
+        list.stream().forEach(System.out::println);
+
+        // count operation
+        long count = list.stream().count();
+        System.out.printf("Count of elements: %d%n", count);
+
+        // collect operation
+        List<String> collectedList = list.stream().collect(Collectors.toList());
+        System.out.printf("Collected list: %s%n", collectedList);
+
+        // anyMatch operation
+        boolean anyMatch = list.stream().anyMatch("a"::equals);
+        System.out.printf("Any match for 'a': %b%n", anyMatch);
+    }
+}
