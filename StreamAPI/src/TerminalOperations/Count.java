881c72dc604509c6a01df66623b3a674cd8459b5
Yerfdog1990 <gouma308@gmail.com>
2024-11-29 04:46:55 +0700

    Count terminal operation.

--- StreamAPI/src/TerminalOperations/Count.java
+++ StreamAPI/src/TerminalOperations/Count.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Count {
     public static void main(String[] args) {
         List<String> list = List.of("Kenya", "Uganda", "Tanzania", "Rwanda", "Burundi");
+        //Count operation: Counts the total number of elements.
         System.out.printf("Original list: %s%n.", list.stream().count());
     }
 }
